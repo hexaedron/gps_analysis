f402b844d083ba6ea063df43ae00a87460540089
hexaedron <kyb.6.granei@yandex.ru>
2016-06-20 19:54:41 +0300

    Update make_list.sh

--- make_list.sh
+++ make_list.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# This script creates the CSV file list
+# to be imported by the SemanticImport function
 
 echo "Filename;Date" > gpx/file_list.csv
 
 for i in gpx/*.gpx;
 do
-	echo  $(basename ${i})";"'"'$(grep time ${i} | head -2| tail -1 | sed -e 's/\s//;s/T/ /;s/[a-zA-Z<>\/]//g')'"' >> gpx/file_list.csv
+  echo  $(basename ${i})";"'"'$(grep time ${i} | head -2| tail -1 | sed -e 's/\s//;s/T/ /;s/[a-zA-Z<>\/]//g')'"' >> gpx/file_list.csv
 done
